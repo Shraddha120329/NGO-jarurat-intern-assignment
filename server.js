f70f3280564e64edb2fd2045615a2c1d88690b93
Shraddha120329 <shraddhayadav1203@gmail.com>
2025-01-25 11:37:19 +0530

    changes

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/resources', resourceRoutes);
 
+app.get('/',(req,res)=>{
+  res.send({
+      activeStatus:true,
+      error:false,
+  })
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
